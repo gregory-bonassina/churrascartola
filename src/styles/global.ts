7e3a6a450d3f3a7b5dc576c2001ca68e69736743
Gregory Bonassina <bonassina3@gmail.com>
2023-04-14 21:37:13 -0300

    remove boxshadow from global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme['red-400']};
+        /* box-shadow: 0 0 0 2px ${(props) => props.theme['red-400']}; */
     }
 
     body {
